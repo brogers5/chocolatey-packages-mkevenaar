a91ba6a75763cadf1c0f6e302dfecdc438f63f13
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-03-19 01:42:17 -0400

    Revert to 70.0.2.0

--- automatic/googledrive/tools/chocolateyInstall.ps1
+++ automatic/googledrive/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-[version] $softwareVersion = '72.0.2.0'
+[version] $softwareVersion = '70.0.2.0'
 $installedVersion = Get-InstalledVersion
 
 if ($installedVersion -eq $softwareVersion -and !$env:ChocolateyForce) {
@@ -15,21 +15,14 @@ else {
     throw 'Windows must be rebooted before installation can be completed!'
   }
 
-  $url = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
-  $checksum = 'C58593C545A036FF2C2B03EA6DB2905CB3E02151F3E3A3539AA1007F8546A201'
-  $checksumType = 'sha256'
-
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
     fileType       = 'exe'
-    url            = $url
-    checksum       = $checksum
-    checksumType   = $checksumType
+    file           = Join-Path -Path $toolsDir -ChildPath 'GoogleDriveSetup.exe'
     softwareName   = 'Google Drive*'
     silentArgs     = "--silent --desktop_shortcut"
     validExitCodes = @(0, 1641, 3010)
   }
 
-  Install-ChocolateyPackage @packageArgs
+  Install-ChocolateyInstallPackage @packageArgs
 }
